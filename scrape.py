23fa4f50ab9e70cd7f7db84306abeb5ec6fe3301
Gram_1302 <abdolahwidadi00@gmail.com>
2024-08-05 21:29:23 +0100

    commit

--- scrape.py
+++ scrape.py
@@ -1,66 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import datetime
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 
-from selenium.webdriver.chrome.service import Service
+# Configure Chrome options
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+options.add_argument('--remote-debugging-port=9222')
 
-#options = Options()
-#options.add_argument('--headless')
-#options.add_argument('--no-sandbox')
-#options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options)
 
-## Specify the correct path to ChromeDriver
-#service = Service('/usr/local/bin/chromedriver')
+# Navigate to the URL
+data = {'IT': {'urls': ['https://example.com/job1', 'https://example.com/job2']}}
 
-driver = webdriver.Chrome()
+job_data = []
 
-# List of URLs to scrape
-urls = ['https://www.jobbank.gc.ca/jobsearch/jobpostingtfw/41480404;jsessionid=C19B7A626490D35D896834EFAD0F254B.jobsearch74?source=searchresults',
- 'https://www.jobbank.gc.ca/jobsearch/jobpostingtfw/41477064;jsessionid=C19B7A626490D35D896834EFAD0F254B.jobsearch74?source=searchresults',
- 'https://www.jobbank.gc.ca/jobsearch/jobpostingtfw/41459537;jsessionid=C19B7A626490D35D896834EFAD0F254B.jobsearch74?source=searchresults' ]
-
-data = []
-
-for url in urls:
+for url in data['IT']['urls']:
     driver.get(url)
     try:
-        apply_now_button = driver.find_element(By.XPATH, '//*[@id="applynowbutton"]')
+        apply_now_button = driver.find_element(By.ID, 'applynowbutton')
         apply_now_button.click()
         time.sleep(1)
-        
-        # Get the page source and parse with BeautifulSoup
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        # Find the email in the <a> element
-        email_element = soup.select_one('#howtoapply > p:nth-of-type(1) > a')
-        email = email_element['href'] if email_element else 'N/A'
-        
-        job_title = soup.select_one('#wb-cont > span:nth-of-type(1)').text if soup.select_one('#wb-cont > span:nth-of-type(1)') else 'N/A'
-        deadline_date = soup.select_one('#applynow > p').text if soup.select_one('#applynow > p') else 'N/A'
-        
+        email_element = driver.find_element(By.XPATH, '//*[@id="howtoapply"]/p[1]/a')
+        email = email_element.get_attribute("href")
+        job_Title = driver.find_element(By.XPATH, '//*[@id="wb-cont"]/span[1]').text
+        deadline_Date = driver.find_element(By.XPATH, '//*[@id="applynow"]/p').text
+
         current_datetime = datetime.datetime.now()
-        current_date = current_datetime.strftime('%Y-%m-%d')  # Format as 'YYYY-MM-DD'
+        current_date = current_datetime.strftime('%Y-%m-%d')
         current_time = current_datetime.strftime('%H:%M:%S')
 
         job_details = {
-            'Job_Title': job_title,
+            'Job_Title': job_Title,
             'Email': email,
-            'Advartized_until': deadline_date,
+            'Advartized_until': deadline_Date,
             'create_date': current_date,
             'create_time': current_time,
-            'date_added': url,  # Assuming `date_added` should be the URL
+            'date_added': url,
             'is_sent': 0
         }
-        data.append(job_details)
+
+        job_data.append(job_details)
     except Exception as e:
-        print(f"Error processing URL {url}: {e}")
+        print(f"Error scraping {url}: {e}")
 
 # Close the browser
 driver.quit()
 
-# Print the results
-for job in data:
+# Print job data
+for job in job_data:
     print(job)
